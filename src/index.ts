8f150f07effa034cd9deef9061c06c7840b8b544
Neil Murphy <barrymun@tcd.ie>
2023-12-10 17:04:45 +0000

    remove redundant code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import { evaluateBoard } from "board";
 import { state } from "state";
 
-// const initialPosition = createSimpleChessPosition(0, false);
-// const result = minimax({
-//   position: initialPosition,
-//   depth: 3,
-//   maximizingPlayer: true
-// });
-// console.log({ result });
-
 const run = () => {
   console.log(state.board.whitePawns);
   console.log(evaluateBoard(state.board));
